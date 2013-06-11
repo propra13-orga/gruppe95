4d924f6c684497769799805440940518f42af1e3
Asma Ben Janete <asma.ben.janete@uni-duesseldorf.de>
2013-06-11 16:45:08 +0200

    to add the image checkoint.png and the class checkpoint

---
+++ src/newgame/checkpoint.java
@@ -0,0 +1,21 @@
+package newgame;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class checkpoint extends Movement {
+
+	Image image;
+
+	ImageIcon u = new ImageIcon("src/Resources/checkpoint.png");			// holt sich die noetigen Grafiken fuer den Charakter
+	
+	public checkpoint(int x, int y){
+		super(x,y);
+		image = u.getImage();
+		this.setImage(image);
+	}
+
+	
+	
+}
