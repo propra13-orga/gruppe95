b4dc9a662ef969529df84bd616c942251623f1c9
inaani <ani.dawidjan@uni-duesseldorf.de>
2013-06-16 21:57:22 +0200

    schwertkampf eingefuert mit passenden positionen bedienung mit der taste v

---
+++ src/newgame/Sword.java
@@ -0,0 +1,93 @@
+package newgame;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+public class Sword{
+
+	private int x,y;
+	Image img;
+	Image image;
+	boolean visible;
+	private int width, height;
+	private int sword_die = 50;
+	
+	public Sword(int x, int y){
+		ImageIcon newSword = new ImageIcon("src/Resources/swordb.png");
+		img = newSword.getImage();
+		visible = true;
+		this.x = x;
+		this.y = y;
+        width = img.getWidth(null);													// fuer Kollision Bild
+        height = img.getHeight(null);
+    }
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+	    return y;
+	}
+	
+	public boolean isVisible() {
+	        return visible;
+	}
+
+	public void setVisible(Boolean visible) {								// setzt Kollision im Board fest
+	        this.visible = visible;
+	}
+
+	public Rectangle getBounds() {											// Kollision Detection
+	        return new Rectangle(x, y, width, height);
+	}
+
+	public Image getImage(){
+		return img;
+	}
+	
+	public void move_r_sword(){
+		x += sword_die;						// Schuss nach rechts mit Geschwindigkeit
+		shot_limit_x1();					
+	}											// dabei wird mit den Funktionen shot_limit_x(bzw.y) darauf
+												// geachtet dass es ueber das Board nicht sichtbar ist
+	public void move_l_sword(){	
+		x -= sword_die;
+		shot_limit_x2();
+	}
+	
+	public void move_u_sword(){								 
+		y -= sword_die;
+		shot_limit_y1();
+	}
+	
+	public void move_d_sword(){								
+		y += sword_die;
+		shot_limit_y2();	
+	}
+	
+	public void shot_limit_x1(){
+		if (x >= 920)
+	         visible = false;						// Wenn der Schuss ausserhalb des Boards ist -> nicht mehr sichtbar
+	}
+	
+	public void shot_limit_y1(){
+		 if (y>= 720)
+	         visible = false;						
+	
+	}
+	
+	public void shot_limit_x2(){
+		 if (x <=0)
+	         visible = false;						
+	}
+	
+	public void shot_limit_y2(){
+		 if (y<=0)
+	         visible = false;						
+	}
+}
+
+   
